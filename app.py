af46522f197ed42f4f9014d73c26331a9f5b21bc
Josef Heers <josef.heers@gbv.de>
2025-05-05 11:44:59 +0200

    Start Flask

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from waitress import serve
 import argparse as AP
+from pathlib import Path
 
 app = Flask(__name__)
 
+def import_file(filename:Flask):
+    # Simulate file import
+    print(f"Importing {filename}")
+    return (f'Import ok',None)
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    err_msg = 'Import failed'
+    if storage_file:=request.files['file']:
+        workFile = f'./upload{Path(storage_file.filename).suffix}'
+        storage_file.save(workFile)
+        success_msg,err_msg = import_file(workFile)
+    # Handle file upload here
+    return render_template('index.html',success=success_msg,error=err_msg)
 
 if __name__ == '__main__':
     parser = AP.ArgumentParser()
