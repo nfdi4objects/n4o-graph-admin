a1de5485e7cf16a1b8631e444fc16d33518c83e4
Josef Heers <josef.heers@gbv.de>
2025-05-06 11:41:19 +0200

    Rest API: /uploadC

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from pathlib import Path
 
 app = Flask(__name__,template_folder='templates', static_folder='static', static_url_path='/assets')
 
-def import_file(src_file:str, work_file:str):
+def import_file(src_file:str, work_file:str, collection='unspecified'):
     # Simulate file import
-    print(f"Importing {src_file}")
-    return (f'Importing {src_file} ok',None)
+    return (f'Importing {src_file} into {collection} - Ok',None)
 
 @app.route('/')
 def index():
@@ -24,6 +23,17 @@ def upload():
         success_msg,err_msg = import_file(src_file,work_file)
     # Handle file upload here
     return render_template('index.html',success=success_msg,error=err_msg)
+
+@app.route('/uploadC/<collection>', methods=['POST'])
+def uploadC(collection):
+    success_msg = 'Import failed'
+    if storage_file:=request.files['file']:
+        src_file = storage_file.filename
+        work_file = f'./upload{Path(src_file).suffix}'
+        storage_file.save(work_file)
+        success_msg,_ = import_file(src_file,work_file,collection)
+    # Handle file upload here
+    return f'message:{success_msg}'
 
 if __name__ == '__main__':
     parser = AP.ArgumentParser()
