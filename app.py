5a756761411c5d288578208ed542d8a5bd8218e3
Josef Heers <josef.heers@gbv.de>
2025-05-14 11:46:47 +0200

    Login page User management Cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, request, make_response, url_for
 from waitress import serve
 import argparse as AP
 from pathlib import Path
@@ -7,29 +7,33 @@ import requests, yaml
 sparql_url = 'http://172.18.0.4:3030/n4/'
 
 app = Flask(__name__,template_folder='templates', static_folder='static', static_url_path='/assets')
+app.secret_key = 'your_secret_key'  # Replace with a strong secret key
+user_collection_name ={'admin':'default'}
+users = {'admin': 'admin123'}
+
 
 def is_RDF_suffix(suffix:str):
     return suffix.lower() in ['.ttl', '.nt', '.nq', '.jsonld', '.json', '.rdf', '.xml']
 
 def import_file(storage_file, collection='default'):
-    files = {'file': (storage_file.filename, storage_file, 'text/plain')}
-    response = requests.post(sparql_url, files=files, params={'graph': f'n4o:{collection}'})
-    return (f'Importing {storage_file.filename} into {collection} - Ok\nanswer={response.text}',None)
+    if collection:
+        files = {'file': (storage_file.filename, storage_file, 'text/plain')}
+        response = requests.post(sparql_url, files=files, params={'graph': f'n4o:{collection}'})
+        return (f'Importing {storage_file.filename} into {collection} - Ok\nanswer={response.text}',None)
 
-@app.route('/')
-def index():
-    return render_template('index.html',collection=collection_name_form)
-
-collection_name_form ='default'
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    global user_collection_name
     err_msg = 'Import failed'
-    global collection_name_form
+    success_msg = coll = None
     if storage_file:=request.files.get('file'):
-        collection_name_form = request.form['collection'] or 'Default'
-        success_msg,err_msg = import_file(storage_file,collection_name_form)
-    return render_template('index.html',success=success_msg,error=err_msg, collection=collection_name_form)
+        if username := request.cookies.get('username','www'):
+            print(f'username={username}')
+            coll = request.form['collection'] or 'Default'
+            user_collection_name[username] = coll
+            success_msg,err_msg = import_file(storage_file,coll)
+    return render_template('index.html',success=success_msg,error=err_msg, collection=coll)
 
 @app.route('/uploadC/<collection>', methods=['POST'])
 def uploadC(collection):
@@ -37,6 +41,38 @@ def uploadC(collection):
     if storage_file:=request.files['file']:
         success_msg,_ = import_file(storage_file,collection)
     return f'message:{success_msg}'
+
+@app.route('/')
+def home():
+    global user_collection_name
+    print(request.cookies)
+    if 'username' in request.cookies:
+        username = request.cookies.get('username')
+        if username not in user_collection_name:
+            user_collection_name[username]='default'   
+        return render_template('index.html',collection=user_collection_name.get(username))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            response = make_response(redirect(url_for('home')))
+            response.set_cookie('username', username)
+            return response
+        else:
+            return 'Invalid username or password'
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.delete_cookie('username')
+    return response
 
 if __name__ == '__main__':
     parser = AP.ArgumentParser()
