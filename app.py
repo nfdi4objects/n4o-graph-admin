88640060a44d4e920268bafa6d243b09248a2de4
Josef Heers <josef.heers@gbv.de>
2025-05-21 15:14:20 +0200

    Refactor

--- app.py
+++ app.py
@@ -1,66 +1,92 @@
 from flask import Flask, render_template, redirect, request, make_response, url_for
 from waitress import serve
 import argparse as AP
-import requests, yaml, hashlib
+import requests
+import yaml
+import hashlib
 
 sparql_url = 'http://172.18.0.4:3030/n4/'
 
-app = Flask(__name__,template_folder='templates', static_folder='static', static_url_path='/assets')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/assets')
 app.secret_key = 'your_secret_key'  # Replace with a strong secret key
-user_collection_name ={'admin':'default','user1':'default_1','guest':'default_2'}
 users = []
 
 
-def is_RDF_suffix(suffix:str):
+def find_user(name):
+    '''Find a user by username in the users list'''
+    for user in users:
+        if user['username'] == name:
+            return user
+    return None
+
+
+def pw_hash(pw_str):
+    '''Hash the password using MD5'''
+    return hashlib.md5(pw_str.encode()).hexdigest()
+
+
+def is_RDF_suffix(suffix: str):
+    '''Check if the file suffix is a valid RDF format'''
     return suffix.lower() in ['.ttl', '.nt', '.nq', '.jsonld', '.json', '.rdf', '.xml']
 
+
 def import_file(storage_file, collection='default'):
+    '''Import a file into the RDF store'''
     if collection:
         files = {'file': (storage_file.filename, storage_file, 'text/plain')}
         response = requests.post(sparql_url, files=files, params={'graph': f'n4o:{collection}'})
-        return (f'Importing {storage_file.filename} into {collection} - Ok\nanswer={response.text}',None)
+        return (f'Importing {storage_file.filename} into {collection} - Ok\nanswer={response.text}', None)
+
 
 @app.route('/info')
 def info():
-    return f'Info: {sparql_url} names = {user_collection_name} user= {[k for k,_ in user_collection_name.items()]}' 
+    '''Display information about the RDF store and users'''
+    return f'Info: {sparql_url} user= {[k['username'] for k,_ in users]}'
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    global user_collection_name
+    '''Handle file upload and import into the RDF store'''
     err_msg = 'Import failed'
     success_msg = coll = None
-    if storage_file:=request.files.get('file'):
-        if username := request.cookies.get('username','www'):
+    if storage_file := request.files.get('file'):
+        if username := request.cookies.get('username', 'www'):
             coll = request.form['collection'] or 'Default'
-            user_collection_name[username] = coll
-            success_msg,err_msg = import_file(storage_file,coll)
-    return render_template('index.html',success=success_msg,error=err_msg, collection=coll)
+            if user := find_user(username):
+                user['collection'] = coll
+            success_msg, err_msg = import_file(storage_file, coll)
+    return render_template('index.html', success=success_msg, error=err_msg, collection=coll)
+
 
 @app.route('/uploadC/<collection>', methods=['POST'])
 def uploadC(collection):
+    '''Handle file upload and import REST API'''
     success_msg = 'Import failed'
-    if storage_file:=request.files['file']:
-        success_msg,_ = import_file(storage_file,collection)
+    if storage_file := request.files['file']:
+        success_msg, _ = import_file(storage_file, collection)
     return f'message:{success_msg}'
+
 
 @app.route('/')
 def home():
-    global user_collection_name
+    '''Render the home page'''
     if 'username' in request.cookies:
         username = request.cookies.get('username')
-        if username not in user_collection_name:
-            user_collection_name[username]='default'   
-        return render_template('index.html',collection=user_collection_name.get(username))
+        coll = 'default'
+        if user := find_user(username):
+            coll = user['collection'] or 'default'
+        return render_template('index.html', collection=coll)
     else:
         return redirect(url_for('login'))
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''Handle user login'''
     if request.method == 'POST':
         username = request.form['username']
-        user = [u for u in users if u['username'] == username]
-        pw = hashlib.md5(request.form['password'].encode()).hexdigest()
-        if user and user[0]['password'] == pw:
+        user = find_user(username)
+        if user and user['password'] == pw_hash(request.form['password']):
             response = make_response(redirect(url_for('home')))
             response.set_cookie('username', username)
             return response
@@ -69,16 +95,21 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
+    '''Handle user logout'''
     response = make_response(redirect(url_for('login')))
     response.delete_cookie('username')
     return response
 
+
 def read_yaml(fname):
-  with open(fname, 'r') as f:
-    config = yaml.safe_load(f)
-    return config
+    '''Read a YAML file and return the data'''
+    with open(fname, 'r') as f:
+        config = yaml.safe_load(f)
+        return config
+
 
 if __name__ == '__main__':
     parser = AP.ArgumentParser()
@@ -92,10 +123,9 @@ if __name__ == '__main__':
         config_data = read_yaml(args.config)
         sparql_url = config_data["fuseki-server"]["uri"]
         user_data = read_yaml('users.yaml')
-        users =user_data['users']
+        users = user_data['users']
     except yaml.YAMLError as err:
         quit(str(err))
-
 
     if args.wsgi:
         serve(app, host="0.0.0.0", **opts)
